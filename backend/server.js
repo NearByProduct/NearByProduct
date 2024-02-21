4e4cd17367e137a14f67d7fef7f14d247ccac775
DEEPAKGUPTA1141421 <deepak2198.be21@chitkara.edu.in>
2024-02-21 17:55:29 +0530

    adding all routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,25 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const shopRoutes = require("./routes/shopRoutes");
 const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const deliveryAgentRoutes = require("./routes/deliveryAgentRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 const errorHandler = require("./utils/errorHanler");
 const connectDb = require("./config/database");
 const cors = require("cors");
+app.use(cookie());
 dotenv.config();
 app.use(fileupload());
-console.log("hello");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cookie());
-app.use(cors());
-
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 // app.use(cors({
 //     origin:"http://localhost:8000",
 //     credentials:true
 // }));
+
 cloudinary.config({
   cloud_name: "drt8pxy1q",
   api_key: "578449198298885",
@@ -35,6 +38,15 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/shop", shopRoutes);
+app.use("/api/v1/order", orderRoutes);
+app.use("/api/v1/rider", deliveryAgentRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
+app.use("/api/v1/getkey", (req, res) =>
+  res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
+);
+
 app.use(errorHandler);
 connectDb();
 const server = app.listen(process.env.PORT, () => {
